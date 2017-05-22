3f2883352fb002c54b3592c400e509a4fdebed03
Larry Hernandez <larryhernanjr@gmail.com>
2017-05-22 11:41:12 -0400

    add changes to AJAX

--- app/controllers/skintEventsController.js
+++ app/controllers/skintEventsController.js
@@ -29,7 +29,7 @@ class SkintEventsController {
       description: req.body.description,
     };
     SkintEventsDAO.create(eventData)
-                     .then((event) => res.status(200).json(event));
+                  .then((event) => res.status(200).json(event));
   }
   static delete(req, res) {
     SkintEventsDAO.delete(req.params.id)
